bcc6bafaa2c440efab9a17743915e10f99b55cff
Rose McKeon <4824372+rosemckeon@users.noreply.github.com>
2024-10-23 17:48:21 +0100

    Base setup with tailwind and favicon

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,scss,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
