9dd72478f965fc73ab4ab59d13924f12cf3b3351
Rose McKeon <4824372+rosemckeon@users.noreply.github.com>
2024-11-29 15:30:16 +0000

    Adjusted routes so only valid scenarios can load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandscapeComponent } from './landscape/landscape.component';
+//import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+type PathMatch = "full" | "prefix" | undefined;
+ 
+// interesting solution to disabling back and refresh if game has started
+// https://stackoverflow.com/questions/42808818/is-it-possible-to-stop-router-navigation-based-on-some-condition
 
 export const routes: Routes = [] = [
     {
@@ -17,11 +22,14 @@ export const routes: Routes = [] = [
         } 
     },
     {
+        path: '',
+        redirectTo: '/A',
+        pathMatch: 'full' as PathMatch
+    },
+    {
         path: '**',
-        component: LandscapeComponent,
-        data: {
-            scenario: 'A'
-        }
+        redirectTo: '/A',
+        pathMatch: 'full' as PathMatch
     }
 
 ];
