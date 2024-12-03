1e8ef4075ae11ec5f01268a6907647218659dbfd
Rose McKeon <4824372+rosemckeon@users.noreply.github.com>
2024-12-03 17:49:47 +0000

    added tooltips to habitat states

--- src/app/habitat/habitat.component.ts
+++ src/app/habitat/habitat.component.ts
@@ -4,7 +4,8 @@ import { Habitat } from '../habitat';
 import { HabitatService } from '../habitat.service';
 import { HabitatResponseComponent } from '../habitat-response/habitat-response.component';
 import { HabitatResponseService } from '../habitat-response.service';
-import { ProgressBarMode, MatProgressBarModule} from '@angular/material/progress-bar';
+//import { ProgressBarMode, MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 //import { MatButtonModule } from '@angular/material/button';
 //import { MatDialog, MatDialogModule, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 //import { NgIconComponent, provideIcons } from '@ng-icons/core';
@@ -13,7 +14,12 @@ import { ProgressBarMode, MatProgressBarModule} from '@angular/material/progress
 @Component({
   selector: 'app-habitat',
   standalone: true,
-  imports: [CommonModule, HabitatResponseComponent, MatProgressBarModule],
+  imports: [
+    CommonModule, 
+    HabitatResponseComponent, 
+    MatTooltipModule
+    //MatProgressBarModule
+  ],
   //providers: [provideIcons({ heroCheckCircleSolid })],
   //changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './habitat.component.html',
@@ -24,7 +30,7 @@ export class HabitatComponent implements OnInit {
   habitatService: HabitatService = inject(HabitatService);
   @Input() habitat!: Habitat;
 
-  mode: ProgressBarMode = 'determinate';
+  //mode: ProgressBarMode = 'determinate';
   //value: number = 0;
   bufferValue: number = 0;
   
@@ -34,19 +40,5 @@ export class HabitatComponent implements OnInit {
     // calculate the overall state value for every habitat loaded
     // this.habitat.stateValue = this.habitatService.calculateHabitatStateValue([this.habitat.state!.wildPollinators!, this.habitat.state!.floralResources!, this.habitat.state!.habitatResources!]);
     //console.log('Responses: ', this.habitat.response)
-  }
-
-  // might do custom mouseover and mouseleave events to show helper info.
-  // could change this for angular material tooltips
-  // need to compare options.
-  showLabel(habitat_id: number, element_id: string):void {
-    console.log("HabitatComponent.showLabel", habitat_id, element_id);
-    //let button = <HTMLButtonElement>document.getElementById('buttonSubmitGlobalChanges');
-    let habitat = document.getElementById("habitat_"+habitat_id);
-    //let label = habitat?.children.  (element_id);
-  }
-
-  hideLabel(habitat_id: number, element_id: string):void {
-    console.log("HabitatComponent.showLabel", habitat_id, element_id);
   }
 }
